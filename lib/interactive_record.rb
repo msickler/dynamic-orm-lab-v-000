a7a00718c362e0a4ab4e170e748199431b9fcc7a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-05 00:50:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,9 +38,9 @@ class InteractiveRecord
     values = []
     self.class.column_names.each do |column_name|
       values << "'#{send(column_name)}'" unless send(column_name).nil?
+    end
       values.join(", ")
-    end
-    end
+  end
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
