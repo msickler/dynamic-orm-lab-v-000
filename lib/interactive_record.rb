fee2b24fe1f635d9078332cbacd637a4d7a4362f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-05 00:41:40 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,6 +30,10 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert 
+    self.class.column_names.delete_if {|column_name| column_name == "id"}.join(", ")
+  end
+
 
 
 
